f769fe9c1db0a64db665bc702b879104ae21bb2a
Matthew Little <zone117x@gmail.com>
2022-02-08 22:47:55 +0100

    ci: build Windows x86 and arm64

--- build-win.bat
+++ build-win.bat
@@ -1,7 +1,7 @@
 @echo off
 
-if NOT "%1" == "x64" if NOT "%1" == "x86" (
-    echo Must specify first argument as x86 or x64
+if NOT "%1" == "x64" if NOT "%1" == "x86" if NOT "%1" == "arm64" (
+    echo Must specify first argument as x64, x86, or arm64
     goto :error
 )
 if "%1" == "x64" (
@@ -10,6 +10,10 @@ if "%1" == "x64" (
 )
 if "%1" == "x86" (
     set arch=Win32
+    set cmake_gen="Visual Studio 17 2022"
+)
+if "%1" == "arm64" (
+    set arch=ARM64
     set cmake_gen="Visual Studio 17 2022"
 )
 
